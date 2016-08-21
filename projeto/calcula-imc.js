768c9ac0d6b293522822104295b477bf3e9ee9fe
Bruno Souza <brunoprogramadorjava>
2016-08-21 16:02:51 -0300

    Atualizando a tela com um objeto paciente

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -5,14 +5,18 @@ tdPeso2 = document.getElementById("peso-2");
 tdAltura2 = document.getElementById("altura-2");
 
 //calcula-imc.js
-var peso = tdPeso2.textContent;
-var altura = tdAltura2.textContent;
 
-if(altura != 0){
-    var imc = peso / (altura * altura); //25
+var tdPeso = document.getElementById("peso-2");
+var tdAltura = document.getElementById("altura-2");
 
-    tdImc = document.getElementById("imc-2");
-    tdImc.textContent = imc;
+var paciente = {peso : tdPeso.textContent, altura : tdAltura.textContent};
+
+if(paciente.altura != 0){
+    var imc = paciente.peso / (paciente.altura * paciente.altura);
+
+    //buscando o elemento "imc-2"
+    var tdImc = document.getElementById("imc-2"); 
+    tdImc.textContent = imc; //atualizando o elemento com o IMC calculado
 
     console.log(imc);
 } else{
