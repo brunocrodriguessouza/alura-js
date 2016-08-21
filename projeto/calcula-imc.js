061f0c4181343bca56a5cc904cba18e3ab8127f9
Bruno Souza <brunoprogramadorjava>
2016-08-21 20:11:48 -0300

    criando uma segundo funcao e separando as responsabilidades

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,32 +1,42 @@
 //calcula-imc.js
 
-function calculaImc(paciente){
-    if(paciente.altura != 0){
-        var imc = paciente.peso / (paciente.altura * paciente.altura);
-        return imc;
-    } else{
-        console.log("Não posso executar uma divisão por 0!");
-    }
-}
+function montaPaciente(pacienteTr){
 
-var trPacientes = document.getElementsByClassName("paciente"); //Array de trs
-
-for(var posicaoAtual = 0; posicaoAtual <= trPacientes.length - 1; posicaoAtual++) {
-
-    var pacienteTr = trPacientes[posicaoAtual];
-
-    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
-    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+	var tdNome = pacienteTr.getElementsByClassName("info-nome")[0]; 
+    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0]; 
     var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
 
-    var pacienteAtual = {nome : tdNome.textContent, peso : tdPeso.textContent, altura : tdAltura.textContent};
+    var paciente = { 
+        nome : tdNome.textContent, 
+        peso : tdPeso.textContent, 
+        altura : tdAltura.textContent,
+        pegaImc: function() { //definindo a função já na declaração do objeto!
 
-    var imc = calculaImc(pacienteAtual);
+            if(this.altura != 0){
+                var imc = this.peso / (this.altura * this.altura);
+                return imc;
+            } else{
+
+                console("Não posso dividir por zero!");
+            }
+        }
+    };    
+   return paciente;
+}
+
+
+var trsPacientes = document.getElementsByClassName("paciente");
+
+for(var posicaoAtual = 0; posicaoAtual <= trsPacientes.length - 1; posicaoAtual++){
+
+    var pacienteTr = trsPacientes[posicaoAtual];
 
     var tdImc = pacienteTr.getElementsByClassName("info-imc")[0]; 
+    var pacienteAtual = montaPaciente(pacienteTr); //Aqui chamamos a nova funcao
+
+    var imc = pacienteAtual.pegaImc();
     tdImc.textContent = imc;
 
-    console.log(imc);
-
+    console.log(imc); 
 }
 
