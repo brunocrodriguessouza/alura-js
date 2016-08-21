9398680f1c2ef7f7f4e5d0969d6130bb985f8046
Bruno Souza <brunoprogramadorjava>
2016-08-21 15:54:04 -0300

    Colocando o resultado do imc calculado na tela

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,9 +1,19 @@
+tdPeso1 = document.getElementById("peso-1");
+tdPeso1 = document.getElementById("altura-1");
+
+tdPeso2 = document.getElementById("peso-2");
+tdAltura2 = document.getElementById("altura-2");
+
 //calcula-imc.js
-var peso = 100;
-var altura = 0; //testando com 0
+var peso = tdPeso2.textContent;
+var altura = tdAltura2.textContent;
 
 if(altura != 0){
-    var imc = peso / (altura * altura);
+    var imc = peso / (altura * altura); //25
+
+    tdImc = document.getElementById("imc-2");
+    tdImc.textContent = imc;
+
     console.log(imc);
 } else{
     console.log("Não posso executar uma divisão por 0!");
